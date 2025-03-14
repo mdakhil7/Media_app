c64bf237795a78e0eb5e268c1f5c4b07885f32e6
mdakhil7 <mdakhilgadwal@gmail.com>
2025-03-14 12:55:59 +0530

    changes of .env files

--- lib/main.dart
+++ lib/main.dart
@@ -3,34 +3,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:socialmedia_app/app.dart';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Load environment variables
+  await dotenv.load(fileName: ".env");
+
   // Lock the app to portrait mode
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+
   await Firebase.initializeApp(
     options: FirebaseOptions(
-      apiKey: 'AIzaSyBe7x4cks2_-NAL_VIMJGVHdJy6M6YwVLI',
-      appId: '1:1072842137623:android:2de8693e98535dc332fc52',
-      messagingSenderId: '1072842137623',
-      projectId: 'foodapp-49d1a',
-      // apiKey: 'AIzaSyCVgHUYPWHMXrX4Lr2oLiQIJjK20wZirmc',
-      // appId: '1:577450694380:android:2343eb1e771850d9ba77c9',
-      // messagingSenderId: '577450694380',
-      // projectId: 'media-fd362',
+      apiKey: dotenv.env['FIREBASE_API_KEY']!,
+      appId: dotenv.env['FIREBASE_APP_ID']!,
+      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+      projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
     ),
   );
-  runApp(
-    MyApp(),
-  );
+
+  runApp(MyApp());
 }
-
-
-//this are comments for pushing code 
-// git checkout -b posting
-// to check the branch git branch
-// git add .
-// git commit -m "Added new feature in posting branch"
-// git push origin posting
